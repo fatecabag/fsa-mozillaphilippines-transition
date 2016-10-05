915c4233f0879f32b0be8e8aff0a144642935d66
Fate Cabag <fatejane.cabag@gmail.com>
2016-10-06 01:42:45 +0800

    One page site for transition

---
+++ script.js
@@ -0,0 +1,7 @@
+$('#start-club-link').click(function() {
+  window.location='https://docs.google.com/forms/d/e/1FAIpQLSfBlMnvOVn6xUMrvBgYWJaEg4npDLUFkhbusLorfZ4BqSJgJQ/viewform';
+});
+
+$('#mozilla-clubs-link').click(function() {
+  window.location='https://campus.mozilla.community/';
+});
